557a0292dff727643fd1b9ad15de0a9efeb5375d
Victor <vgm.maraccini@gmail.com>
2016-06-01 18:00:30 -0300

    Improve chart data object

--- NUCharts/Classes/Model/Data/NUChartData.h
+++ NUCharts/Classes/Model/Data/NUChartData.h
@@ -10,12 +10,21 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface NUChartData : NSObject
-@property (nonatomic, strong) NSArray<NSNumber *> *xValues;
-@property (nonatomic, strong) NSArray<NSNumber *> *yValues;
+@interface NUChartData : NSObject<NSCopying>
+@property (nonatomic, readonly) NSArray<NSValue *>* points;
 
 - (instancetype)initWithxValues:(NSArray<NSNumber *>*)xValues
                         yValues:(NSArray<NSNumber *>*)yValues;
+
+- (instancetype)initWithCGPointArray:(NSArray<NSValue *>*)points;
+
+
+@property (nonatomic, readonly) CGFloat maximumX;
+@property (nonatomic, readonly) CGFloat maximumY;
+
+@property (nonatomic, readonly) CGFloat minimumY;
+@property (nonatomic, readonly) CGFloat minimumX;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- NUCharts/Classes/Model/Data/NUChartData.m
+++ NUCharts/Classes/Model/Data/NUChartData.m
@@ -9,13 +9,34 @@
 #import "NUChartData.h"
 
 @implementation NUChartData
+
 - (instancetype)initWithxValues:(NSArray<NSNumber *>*)xValues
                         yValues:(NSArray<NSNumber *>*)yValues {
+    NSAssert(xValues.count == yValues.count, @"X and Y values should have the same count");
+
+    NSMutableArray *points = [NSMutableArray new];
+    for (int i = 0; i < xValues.count; i++) {
+        CGPoint point = CGPointMake(xValues[i].doubleValue, yValues[i].doubleValue);
+        [points addObject:[NSValue valueWithCGPoint:point]];
+    }
+
+    return [self initWithCGPointArray:points];
+}
+
+- (instancetype)initWithCGPointArray:(NSArray<NSValue *>*)points {
     self = [super init];
     if (self) {
-        _xValues = xValues;
-        _yValues = yValues;
+
+        for (NSValue *wrapped in points) {
+            CGPoint point = wrapped.CGPointValue;
+            _minimumX = MIN(_minimumX, point.x);
+            _minimumY = MIN(_minimumY, point.y);
+            _maximumX = MAX(_maximumX, point.x);
+            _maximumY = MAX(_maximumY, point.y);
+        }
+        _points = points;
     }
     return self;
 }
+
 @end
